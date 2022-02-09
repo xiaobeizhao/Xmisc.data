2794937be68a414f44a2d15dc952f0ae86a9be52
xiaobeizhao <xiaobei@login01.uthsc.edu>
2022-02-09 11:52:48 -0600

    first commit

---
+++ R/Xmisc.data-package.R
@@ -0,0 +1,34 @@
+
+## ************************************************************************
+## Xmisc.data-package.R: Roxygen2 documentation file for the Xmisc.data package.
+## 
+## 
+## (c) Xiaobei Zhao
+## 
+## Thu Jun 12 23:59:34 EDT 2014 -0400 (Week 23)
+## ## To generate
+## roxygen2::roxygenize(".")
+##
+## 
+## Reference: 
+## 
+## 
+## ************************************************************************
+
+
+##' Xiaobei's collected datasets for R projects
+##' 
+##' Listed are a few highlighted sets
+##' \itemize{
+##' \item \code{\link{chess.openings}}
+##' }
+##'
+##' You may find the latest version of \code{Xmisc.data} at \url{https://github.com/xiaobeizhao/Xmisc.data}.
+##' @title Xiaobei's collected datasets for R projects
+##' @name Xmisc.data-package
+##' @docType package
+##' @author Xiaobei Zhao
+##' @references Xiaobei Zhao (2020)
+##' @keywords package
+NULL
+
---
+++ R/data.R
@@ -0,0 +1,38 @@
+
+## ************************************************************************
+## 
+## 
+## 
+## (c) Xiaobei Zhao
+## 
+## Mon Feb 07 18:04:22 CST 2022 -0600 (Week 06)
+## 
+## 
+## Reference: 
+## 
+## 
+## ************************************************************************
+
+
+
+##' Chess Openings in Lichess Opening Tournament Schedule
+##'
+##' A dataset containing the original index, ECO codes and opening names
+##' of 131 Chess Openings in Lichess Opening Tournament Schedule
+##' 
+##' @title Chess Openings in Lichess Opening Tournament Schedule
+##' @return data.table
+##' @author
+##' @docType data
+##' @keywords datasets
+##' @name chess.openings
+##' @usage data(chess.openings)
+##' @format A data table with 131 rows and 3 columns:
+##' \itemize{
+##'   \item{Index, as in original order}
+##'   \item{ECO, ECO (The Encyclopaedia of Chess Openings) codes}
+##'   \item{Opening, Opening names}
+##' }
+##' @source \url{https://benediktwerner.github.io/lichess-themed-tournament-schedule/}
+##' 
+NULL
